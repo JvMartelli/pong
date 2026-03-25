8318f43aa13767e671bac8d9167f17aea471f8b5
João Vitor Martelli <jvmartelli05@gmail.com>
2026-03-24 21:47:17 -0300

    fix: corrige tremido da IA com zona morta no movimento

--- ai_controller.py
+++ ai_controller.py
@@ -4,10 +4,12 @@ from ball import Bola
 
 class ControladorIA:
 
+    ZONA_MORTA = 6
+
     def atualizar(self, raquete: Raquete, bola: Bola):
         centro_raquete = raquete.y + raquete.altura // 2
 
-        if centro_raquete < bola.y:
+        if centro_raquete < bola.y - self.ZONA_MORTA:
             raquete.mover_baixo()
-        elif centro_raquete > bola.y:
+        elif centro_raquete > bola.y + self.ZONA_MORTA:
             raquete.mover_cima()
